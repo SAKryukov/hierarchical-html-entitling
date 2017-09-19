63c22eff4f4c645c69b550bc284eb12115dffb04
SAKryukov <github@sakryukov.org>
2017-09-19 02:44:30 -0400

    Code: implemented new feature, auto-sizing.js of auto-generated headings

---
+++ docs/auto-numbering/auto-sizing.js
@@ -0,0 +1,45 @@
+/*
+  
+   Hierarchical HTML Entitling
+  
+   Copyright (c) Sergey A Kryukov, 2017
+  
+   http:  www.SAKryukov.org
+   http:  www.codeproject.com/Members/SAKryukov
+   https:  github.com/SAKryukov
+   https:  github.com/SAKryukov/hierarchical-html-entitling
+  
+*/
+
+function autoSizeHeadings(min, max, levels, headerTagTOC) {
+
+    if (max < min) return;
+    if (!levels || levels < 1) levels = 1;
+    const step = levels > 1 ? (max - min) / (levels - 1) : 0;
+    const ruleSet = [];
+
+    for (let index = 0; index < levels; ++index) {
+        const rule = {};
+        const size = max - index * step;
+        rule.section = "section:before {font-size:" + size + "%}";
+        rule.header = "section>header {font-size:" + size + "%}";
+        for (let level = 0; level < index; ++level) {
+            rule.section = "section>" + rule.section;
+            rule.header = "section>" + rule.header;
+        } //loop
+        ruleSet.push(rule);
+    } //loop
+
+    (function setStyles(){
+        const styleElement = document.createElement('style');
+        document.head.appendChild(styleElement);
+        styleSheet = styleElement.sheet;
+        for (let rule of ruleSet) {
+            styleSheet.insertRule(rule.section, 0);    
+            styleSheet.insertRule(rule.header, 0);    
+        } //loop
+        if (headerTagTOC)
+            styleSheet.insertRule(headerTagTOC + " {font-size:" + max + "%}", 0);
+    })();
+
+} //autoSizeHeadings
